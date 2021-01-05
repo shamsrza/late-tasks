564b3920d04916a3e5d9ebdee89de4ce23c7397d
Shams Rzayeva <66381642+shamsrza@users.noreply.github.com>
2021-01-05 10:13:44 +0100

    collections-17-12-2020 TASK

---
+++ collections-17-12-2020_task_1/ConsoleApp25/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            dict1.Add("Hormetli istifadeci! Zehmet olmasa olke adi qeyd edin: ", "Hormetli istifadeci! Zehmet olmasa olkeye aid paytaxt qeyd edin: ");
+
+            List<string> list = new List<string>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+
+                foreach (KeyValuePair<string, string> kvp1 in dict1)
+                {
+                    Console.WriteLine(kvp1.Key);
+                    list.Add(Console.ReadLine());
+                }
+
+                foreach (KeyValuePair<string, string> kvp1 in dict1)
+                {
+                    Console.WriteLine(kvp1.Value);
+                    list.Add(Console.ReadLine());
+                }
+
+            }
+
+            Dictionary<string, string> dict3 = new Dictionary<string, string>();
+            dict3.Add("Hormetli istifadeci! Zehmet olmasa paytaxtini tapmaq istediyiniz olkenin adini qeyd edin: ", "Baki");
+            foreach (KeyValuePair<string, string> kvp2 in dict3)
+            {
+                Console.WriteLine(kvp2.Key);
+                string ask = Console.ReadLine();
+
+                if (ask == "Azerbaycan")
+                {
+                    Console.WriteLine(kvp2.Value);
+                    Console.ReadLine();
+                }
+                else if (ask == "all")
+                {
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+
+            Console.WriteLine("Yeni paytaxt axtarılsın? Y/N");
+            string newRequest = Console.ReadLine();
+            if (newRequest == "Y")
+            {
+                Console.WriteLine("Hormetli istifadeci! Zehmet olmasa yeni axtaris ucun olke adini qeyd edin: ");
+                Console.ReadLine();
+            }
+            else if (newRequest == "N")
+            {
+
+            }
+
+        }
+    }
+}
---
+++ collections-17-12-2020_task_2_3_and_4/ConsoleApp26/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp26
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 2
+            /* Queue<string> fruits = new Queue<string>();
+             fruits.Enqueue("apple");
+             fruits.Enqueue("orange");
+             fruits.Enqueue("lemon");
+             fruits.Enqueue("melon");
+             fruits.Enqueue("kiwi");
+             fruits.Enqueue("banana");
+             fruits.Enqueue("cherry");
+             fruits.Enqueue("mango");
+             fruits.Enqueue("pineapple");
+             fruits.Enqueue("strawberry");
+             fruits.Enqueue("grape");
+
+
+             if (fruits.Count > 10)
+             {
+                 fruits.Dequeue();
+                 fruits.Enqueue("avocado");               
+             }
+
+             foreach (var item in fruits)
+             {
+                 Console.WriteLine(item);
+             } */
+
+
+
+
+            // ***********   ************   **************   ***************
+
+
+
+            //TASK 3
+            /* List<int> numbers = new List<int>();
+            
+            Console.WriteLine("Please enter 10 different numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(Convert.ToInt32(Console.ReadLine())); 
+            }
+
+
+            for (int i = 0; i < 5 ; i++)
+            {
+                int temp = numbers[i];
+                numbers[i] = numbers[^(i + 1)];
+                numbers[^(i + 1)] = temp;
+            }
+            foreach (int item in numbers)
+            {
+                Console.Write($"{item} ");
+            } */
+
+
+
+            // ***********   ************   **************   ***************
+
+
+            
+            //TASK 4
+            /* HashSet<int> numbers = new HashSet<int>();
+            Console.WriteLine("Please enter 5 different numbers: ");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.Write($"{item} ");
+            }
+            */
+        }
+
+    }        
+}
