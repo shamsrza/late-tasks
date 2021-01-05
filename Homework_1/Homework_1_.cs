50b3564218ef4b92bec21a9bb4c601e6c232c037
Shams Rzayeva <66381642+shamsrza@users.noreply.github.com>
2021-01-05 09:59:58 +0100

    task

---
+++ Homework_1/Homework_1_.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Homework_15_Dec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /*a = 3 rəqəmli ədəd daxil edin (rəqəm sayı 3 olmazsa proqram yeni rəqəm istəsin)
+         b = 5 rəqəmli ədəd daxil edin (rəqəm sayı 5 olmazsa proqram yeni rəqəm istəsin)
+         a-nın İLK 2 rəqəmini, b-nin isə SON 4 rəqəmini string kimi birləşdirib daha sonra rəqəmə çevirin.
+         Alınan ədədin son 3 rəqəmindən ilk 3 rəqəmini çıxın.
+         Nəticə mənfi olarsa proqram başdan başlamalıdır.
+         Misal üçün:
+         a = 142; b = 25152;
+
+         a-nın ilk 2 rəqəmi: 14. b-nin son 3 rəqəmi: 5152.
+
+         String kimi birləşdirdikdə 145152 alırıq. Daha sonra aldığımız ədədin son 3 rəqəmindən ilk 3 rəqəmini çıxırıq:
+
+         145152: son 3 rəqəmi: 152 ilk 3 rəqəmi: 145
+
+         145-152=-7. Cavab mənfi alındığı üçün proqram yenidən çalışmalıdır.
+         */
+
+
+            START:
+            tryAgain1:
+            Console.Write("Please enter 3-digits number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int attemptLimit = 1;
+            
+            for (int i = 0; i < attemptLimit; i++)
+            {
+                if (num1 >= 100 && num1 <= 999)
+                {
+                   Console.Write("Correct!");
+                }
+                else if (num1 < 100 || num1 > 999)
+                {
+                    Console.Write("Invalid Number. Please try again: ");
+                } else
+                {
+                    goto tryAgain1;
+                }
+                Console.ReadLine();
+            }
+
+            tryAgain2:
+            Console.Write("Please enter 5-digits number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < attemptLimit; i++)
+            {
+                if (num2 >= 10000 && num2 <= 99999)
+                {
+                    Console.Write("Correct!");
+                } else if (num2 < 10000 || num2 > 99999) 
+                {
+                    Console.Write("Invalid Number. Please try again: ");
+                }
+                else
+                {
+                   goto tryAgain2;
+                }
+                Console.ReadLine();
+            }
+
+            string a = num1.ToString();
+            a = a.Remove(2, 1);
+            Console.WriteLine(a);
+
+            string b = num2.ToString();
+            b = b.Substring(1);
+            Console.WriteLine(b);
+
+            var s = String.Concat(a, b);
+            //Console.WriteLine(s);
+
+            int resultNum = Int32.Parse(s);
+            Console.WriteLine(resultNum);
+            resultNum = (resultNum % 1000) - ((resultNum - (resultNum % 1000)) / 1000);
+            if ( resultNum > 0)
+            {
+                Console.WriteLine(resultNum);
+            }
+            else
+            {
+                goto START;
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+    }
---
+++ Homework_2__/Homework_2__/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Homework_2__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        /* İstifadəçidən aşağıdaki şərtlərə uyğun İNGİLİSCƏ söz yazılması istənilsin:
+           a) Söz 5 hərfdən ibarət olmalıdır.
+
+           b) Sözdə "a" hərfindən istifadə olunmamalıdır.
+
+           c) Sözdə ən azı bir ədəd "b" hərfi olmalıdır.
+
+           d) Sözün sonunda mütləq nöqtə (".") olmalıdır.
+        Əgər şərtlərdən biri pozularsa proqram yenidən başlasın, əks halda davam etsin.
+        Sözdə olan bütün "u" hərflərini "k" ilə əvəz et.
+        Sözdə olan bütün "z" hərflərini sil.
+        Sözdə olan bütün "o" hərflərini böyük et ("O").  
+        Sözdə olan bütün "L" hərflərini balaca et ("l").
+        Sözün son hecasını ilk hecaya bitişdir.
+        Məsələn: "urban" olmalıdır "banur". "ur" və "ban" hecalarında "ban" keçir önə. - BUNU DEDIYINIZ KIMI SKIP ELEDIM */
+
+        START:
+            Console.Write("Please enter 5-letter word: ");
+            string word = Console.ReadLine();
+            bool a = word.Contains('a');
+            bool b = word.Contains('b');
+            bool u = word.Contains('u');
+            bool z = word.Contains('z');
+            bool o = word.Contains('o');
+            bool L = word.Contains('L');
+
+
+           for (int i = 0; i < 1; i++)
+            {
+                if (word.Length == 6 && !a && b == true && word[word.Length - 1] == '.')
+                {
+                    if (u == true || z == true || o == true || L == true )
+                    {
+                        word = word.Replace('u', 'k');
+                        word = word.Remove(word.IndexOf('z'));
+                        word = word.Replace('o', 'O');
+                        word = word.Replace('L', 'l');
+                        Console.WriteLine(word);
+                    }
+       
+                }
+                else
+                {
+                    goto START;
+                }
+            }
+                
+
+
+        }
+    }
+}
