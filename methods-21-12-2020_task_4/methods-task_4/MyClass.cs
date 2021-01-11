464a48ea17c37ec3f1ec8a2605c45543f7d371a6
Shams Rzayeva <66381642+shamsrza@users.noreply.github.com>
2021-01-11 21:55:13 +0100

    NEW_methods-21-12-2020 TASK

---
+++ methods-21-12-2020_task_4/methods-task_4/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace methods_task_4
+{
+    public static class MyClass
+    {
+        public static string GetLetterCount(this string s, char letter)
+        {
+            int cnt = s.Count(c => c == letter);
+            string count = Convert.ToString(cnt);
+            return count;
+        }
+    }
+}
---
+++ methods-21-12-2020_task_4/methods-task_4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace methods_task_4
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            List<string> wordsList = new List<string>();
+            List<string> list2 = new List<string>();
+            string s = "";
+            char c = 'y';
+
+        firstRequest:
+            Console.WriteLine();
+            Console.WriteLine("Zehmet olmasa sozler daxil edin.");
+            for (int i = 0; i < 3; i++)
+            {
+                s = Console.ReadLine();
+                wordsList.Add(s);
+            }
+            
+            Console.WriteLine("Daha 3 soz yazmaq isteyirsiz? Y/N");
+            ConsoleKey pressedKey = Console.ReadKey().Key;
+            if (pressedKey == ConsoleKey.Y)
+            {
+                goto firstRequest;
+            }
+            else if (pressedKey == ConsoleKey.N)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Zehmet olmasa bir herf daxil edin.\nEger secim etmek istemirsinizse  ~ duymesini secin.");
+                if (pressedKey == ConsoleKey.Oem3)
+                {
+
+                }
+                else if (pressedKey != ConsoleKey.Oem3)
+                {
+                    c = char.Parse(Console.ReadLine());
+                }
+                else
+                {
+
+                }
+
+            }
+
+            foreach (string item in wordsList)
+            {
+                Console.WriteLine($"{item} - {item.GetLetterCount(c)}");
+
+            }
+
+        }
+
+    } 
+
+}
