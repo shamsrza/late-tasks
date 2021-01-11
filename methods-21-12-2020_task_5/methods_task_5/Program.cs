90132e943328ac7bdfb80fd86e7c2102e1583a74
Shams Rzayeva <66381642+shamsrza@users.noreply.github.com>
2021-01-11 22:49:42 +0100

    methods-21-12-2020 TASK

---
+++ methods-21-12-2020_task_5/methods_task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace methods_task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 25;
+            AddData(ref a);
+            Console.WriteLine(a);
+            
+        }
+
+        public static void AddData(ref int x)
+        {
+            x = x + 10;
+        }
+    }
+}
